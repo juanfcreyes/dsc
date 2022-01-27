4b6d4f9615f2a144a5936f39033dfb7d329e1fa9
Juan Castillo <juanfcreyes@hotmail.com>
2022-01-27 14:37:42 -0500

    button version 3

--- scope/ui/button/button.composition.tsx
+++ scope/ui/button/button.composition.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
 import { Button } from './button';
 
-export const BasicButton = () => {
+export const PrimaryButton = () => {
   return (
-    <Button>hello world!</Button>
+    <Button >hello world!</Button>
+  );
+}
+
+export const SecondaryButton = () => {
+  return (
+    <Button primary={false}>hello world!</Button>
+  );
+}
+
+
+export const SmmallButton = () => {
+  return (
+    <Button size='small'>Small</Button>
+  );
+}
+
+export const MediumButton = () => {
+  return (
+    <Button size='medium'>Medium</Button>
+  );
+}
+
+export const LargeButton = () => {
+  return (
+    <Button size='large'>Large</Button>
   );
 }
--- scope/ui/button/button.tsx
+++ scope/ui/button/button.tsx
@@ -1,16 +1,37 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, FC } from 'react';
+import styled from 'styled-components';
+
+
+const sizes = {
+  'small' : '8px 24px;',
+  'medium': '16px 24px;',
+  'large': '24px 24px;'
+}
+
+const StyleButton = styled.button`
+  /* Adapt the colors based on primary prop */
+  cursor: pointer;
+  background: ${ ({primary}) => primary ? "#ffdd00" : "#ebeef5"};
+  color: #0f265c;
+  font-size: 1em;
+  margin: 1em;
+  padding: ${({size}) => sizes[size]} 
+  border: 0px;
+  border-radius: 4px;
+`;
+
+export type ButtonSizes = 'small' | 'medium' | 'large'
 
 export type ButtonProps = {
-  /**
-   * a node to be rendered in the special component.
-   */
   children?: ReactNode;
+  primary?: boolean;
+  size?: ButtonSizes;
 };
 
-export function Button({ children }: ButtonProps) {
+export const Button : FC<ButtonProps> = ({ children, primary = true, size = 'medium' }) => {
   return (
-    <div>
+    <StyleButton primary={primary} size={size}>
       {children}
-    </div>
+    </StyleButton>
   );
 }
